1271f7eff3a3f3adec2ef07facc3cd53e8c83682
dcereijodo <dcereijo@digitalorigin.com>
2019-11-08 19:23:41 +0100

    generate add partition statement in batches

--- macros/helpers.sql
+++ macros/helpers.sql
@@ -36,3 +36,52 @@
     {{return(ddl)}}
 
 {% endmacro %}
+
+{#
+  Generates a series of alter statements to add a batch of partitions to a table.
+  Ideally it would require a single alter statement to add all partitions, but
+  Amazon imposes a limit of 100 partitions per alter statement. Therefore we need
+  to generate multiple altes when the number of partitions to add exceeds 100.
+
+  Arguments:
+    - table (string): The name of the table to generate the partitions for.
+    - source_external_location (string): Base location of the external table. Paths
+        in the 'partitions' argument are specified relative to this location.
+    - partitions (list): A list of partitions to be added to the external table.
+        Each partition is represented by a dictionary with the keys:
+          - partition_by (list): A set of columns that the partition is affected by
+              Each column is represented by a dictionary with the keys:
+                - name: Name of the column
+                - value: Value of the column
+          - path (string): The path to be added as a partition for the particular
+              combination of columns defined in the 'partition_by'
+#}
+{% macro add_partitions(table, source_external_location, partitions) %}
+
+  {{ log("Generating ADD PARTITION statement for partition set \n" ~ partitions, info=True) }}
+
+  {% if partitions|length > 0 %}
+
+      alter table {{ table }} add
+
+    {% for partition in partitions %}
+
+      {% if loop.index % 100 == 0 %}
+
+        ; -- close alter statement and open a new one
+        alter table {{ table }} add
+
+      {% endif %}
+
+        partition ({%- for part in partition.partition_by -%}{{ part.name }}='{{ part.value }}'{{',' if not loop.last}}{%- endfor -%})
+        location '{{ source_external_location }}{{ partition.path }}/'
+
+    {% endfor %}
+
+  {% else %}
+
+    {{ log("No partitions to be added", info=True) }}
+
+  {% endif %}
+
+{% endmacro %}
