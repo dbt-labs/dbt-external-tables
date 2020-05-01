16a836fd7aa038353296ec925ac4d92ded9170a9
Jeremy Cohen <jeremy@fishtownanalytics.com>
2020-05-01 19:03:45 -0400

    Update sample analysis

--- sample_analysis/external_sources_dry_run.sql
+++ sample_analysis/external_sources_dry_run.sql
@@ -2,9 +2,9 @@
     
     {%- if node.resource_type == 'source' and node.external.location != none -%}
     
-        {{- dbt_utils.log_info('Staging external source ' ~ node.schema ~ '.' ~ node.identifier) -}}
+        {{ 'Staging external source ' ~ node.schema ~ '.' ~ node.identifier) }}
         
-        {%- set run_queue = dbt_external_tables.get_external_build_plan(node).split(';') -%}
+        {%- set run_queue = dbt_external_tables.get_external_build_plan(node) -%}
         
         {%- for q in run_queue %}
             {{ q }}
