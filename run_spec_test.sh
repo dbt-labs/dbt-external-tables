fe874cf7e3bd69a138361105728e1fbf6387b977
dm03514 <dm03514@gmail.com>
2020-11-28 17:49:10 -0500

    cue test command sudo cp

--- run_spec_test.sh
+++ run_spec_test.sh
@@ -3,7 +3,7 @@
 if [ "$DBT_EXTERNAL_TABLES_CI_INSTALL_CUE" == "1" ]; then
     wget https://github.com/cuelang/cue/releases/download/v0.2.2/cue_0.2.2_Linux_x86_64.tar.gz
     tar xvfz cue_0.2.2_Linux_x86_64.tar.gz
-    cp cue /usr/local/bin
+    sudo cp cue /usr/local/bin
 fi
 
 cue vet sample_sources/spark.yml spec.cue
