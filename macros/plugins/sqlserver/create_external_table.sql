7bbbe328d94191946f7fd4e2902361f89dbb22f4
Anders Swanson <swanson.anders@gmail.com>
2020-12-01 17:12:58 -0800

    support RDBMS

--- macros/plugins/sqlserver/create_external_table.sql
+++ macros/plugins/sqlserver/create_external_table.sql
@@ -3,6 +3,25 @@
     {%- set columns = source_node.columns.values() -%}
     {%- set external = source_node.external -%}
 
+    {% set query %}
+        SELECT Type_desc
+        FROM [sys].[external_data_sources]
+        WHERE Name = '{{external.data_source}}'
+    {% endset %}
+    {% set results = run_query(query) %}
+    {% set datasource_type = results.columns[0].values()[0] %}
+
+    {%- if datasource_type == "HADOOP" -%}
+    {% set dict = {'DATA_SOURCE': external.data_source,
+                    'LOCATION' : external.location, 
+                    'FILE_FORMAT' : external.file_format, 
+                    'REJECT_TYPE' : external.reject_type, 
+                    'REJECT_VALUE' : external.reject_value} -%}
+    {%- elif datasource_type == "RDBMS" -%}
+    {% set dict = {'DATA_SOURCE': external.data_source,
+                    'SCHEMA_NAME' : external.schema_name, 
+                    'OBJECT_NAME' : external.object_name} -%}
+    {%- endif %}
     {% if external.ansi_nulls is true -%} SET ANSI_NULLS ON; {%- endif %}
     {% if external.quoted_identifier is true -%} SET QUOTED_IDENTIFIER ON; {%- endif %}
 
@@ -15,11 +34,6 @@
         {% endfor %}
     )
     WITH (
-        {% set dict = {'DATA_SOURCE': external.data_source,
-                       'LOCATION' : external.location, 
-                       'FILE_FORMAT' : external.file_format, 
-                       'REJECT_TYPE' : external.reject_type, 
-                       'REJECT_VALUE' : external.reject_value} -%}
         {%- for key, value in dict.items() %}
             {{key}} = {% if key == "LOCATION" -%} '{{value}}' {%- elif key in ["DATA_SOURCE","FILE_FORMAT"] -%} [{{value}}] {%- else -%} {{value}} {%- endif -%}
             {{- ',' if not loop.last -}}
