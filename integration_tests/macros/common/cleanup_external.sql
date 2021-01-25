557bf47c292515e57925b74376f5a2c3828d2954
Jeremy Cohen <jeremy@fishtownanalytics.com>
2021-01-25 11:51:10 +0100

    Add back code lost in merge

--- integration_tests/macros/common/cleanup_external.sql
+++ integration_tests/macros/common/cleanup_external.sql
@@ -6,14 +6,3 @@
     {% do log('No cleanup necessary, skipping', info = true) %}
     {# noop #}
 {% endmacro %}
-
-{% macro snowflake__cleanup_external() %}
-
-    {% set unset_autocommit %}
-        alter user {{ target.user }} unset autocommit;
-    {% endset %}
-
-    {% do log('Unsetting autocommit parameter for user ' ~ target.user, info = true) %}
-    {% do run_query(unset_autocommit) %}
-    
-{% endmacro %}
