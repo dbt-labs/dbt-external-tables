ca279960f28ebdccef3edccefef9fd4f3c43441f
Chaerin Lee <chaerin.lee@avanade.com>
2021-02-22 16:30:10 -0700

    small fix

--- macros/plugins/sqlserver/get_external_build_plan.sql
+++ macros/plugins/sqlserver/get_external_build_plan.sql
@@ -24,6 +24,6 @@
 
 {% macro synapse__get_external_build_plan(source_node) %}
 
-    {% do return(sqlserver__get_external_build_plan(source_node))
+    {% do return(sqlserver__get_external_build_plan(source_node) %)
 
 {% endmacro %}
