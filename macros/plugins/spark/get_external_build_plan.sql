cb01e5e941021432d7f28940ebd36d5be35420d7
Cor Zuurmond <jczuurmond@protonmail.com>
2021-12-15 14:28:11 +0100

    Add recover partitions statement to build plan

--- macros/plugins/spark/get_external_build_plan.sql
+++ macros/plugins/spark/get_external_build_plan.sql
@@ -19,6 +19,13 @@
         {% set build_plan = build_plan + dbt_external_tables.refresh_external_table(source_node) %}
     {% endif %}
 
+    {% set recover_partitions = spark__recover_partitions(source_node) %}
+    {% if recover_partitions|length > 0 %}
+    {% set build_plan = build_plan + [
+        recover_partitions
+    ] %}
+    {% endif %}
+
     {% do return(build_plan) %}
 
 {% endmacro %}
