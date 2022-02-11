be01e1b7dd6b293a86c70712e569861c577d8951
Daniel Cortez <daniel@jyde.io>
2022-02-11 13:41:52 +0100

    Remove debugging flags

--- run_test.sh
+++ run_test.sh
@@ -38,6 +38,6 @@ echo "Starting integration tests"
 dbt deps --target $1
 dbt seed --full-refresh --target $1
 dbt run-operation prep_external --target $1
-dbt  -dx --printer-width 500 run-operation stage_external_sources --var 'ext_full_refresh: true' --target $1
-dbt  -dx --printer-width 500 run-operation stage_external_sources --target $1
+dbt run-operation stage_external_sources --var 'ext_full_refresh: true' --target $1
+dbt run-operation stage_external_sources --target $1
 dbt test --target $1
