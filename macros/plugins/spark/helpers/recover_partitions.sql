fbc766f94dd0ed52fbfba0e526c7de8d8de57e40
Jarno Rajala <jarno.rajala@hurtigruten.com>
2022-03-01 09:00:05 +0200

    Recover partitions for Spark
    Add 'recover_partitions' option for Spark to run ALTER TABLE RECOVER PARTITIONS even if partitions are not explicitly specified. This makes using inferred partitions possible.

--- macros/plugins/spark/helpers/recover_partitions.sql
+++ macros/plugins/spark/helpers/recover_partitions.sql
@@ -2,7 +2,7 @@
     {# https://docs.databricks.com/sql/language-manual/sql-ref-syntax-ddl-alter-table.html #}
 
     {% set ddl %}
-    {%- if source_node.external.partitions and source_node.external.using and source_node.external.using|lower != 'delta' -%}
+    {%- if (source_node.external.partitions or source_node.external.recover_partitions) and source_node.external.using and source_node.external.using|lower != 'delta' -%}
         ALTER TABLE {{ source(source_node.source_name, source_node.name) }} RECOVER PARTITIONS
     {%- endif -%}
     {% endset %}
