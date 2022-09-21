51a16efcc5d0b4bd8bd7063d491f3a7e8ed60f83
jeremyyeo <jeremyyeo@gmail.com>
2022-09-21 21:27:31 +1200

    fix comments

--- macros/plugins/spark/helpers/recover_partitions.sql
+++ macros/plugins/spark/helpers/recover_partitions.sql
@@ -18,8 +18,10 @@
 {% endmacro %}
 
 {% macro default__recover_partitions(source_node) %}
-    {# We're dispatching this macro so that users can override it if required on other adapters
-    but this will work for spark/databricks.#}
+    /*{# 
+        We're dispatching this macro so that users can override it if required on other adapters
+        but this will work for spark/databricks. 
+    #}*/
 
     {{ exceptions.raise_not_implemented('recover_partitions macro not implemented for adapter ' + adapter.type()) }}
 {% endmacro %}
