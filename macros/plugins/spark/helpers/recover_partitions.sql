56fd1f0d66850247d44c8fb04b33b9e80d8c52f3
jeremyyeo <jeremyyeo@gmail.com>
2022-09-21 21:15:26 +1200

    fix self reference

--- macros/plugins/spark/helpers/recover_partitions.sql
+++ macros/plugins/spark/helpers/recover_partitions.sql
@@ -1,12 +1,18 @@
 {% macro spark__recover_partitions(source_node) %}
     {# https://docs.databricks.com/sql/language-manual/sql-ref-syntax-ddl-alter-table.html #}
 
-    {% set ddl %}
     {%- if source_node.external.partitions and source_node.external.using and source_node.external.using|lower != 'delta' -%}
-        ALTER TABLE {{ source(source_node.source_name, source_node.name) }} RECOVER PARTITIONS
+        {% set ddl %}
+            ALTER TABLE {{ source(source_node.source_name, source_node.name) }} RECOVER PARTITIONS
+        {% endset %}
+    {%- else -%}
+        {% set ddl = none %}
     {%- endif -%}
-    {% endset %}
 
     {{return(ddl)}}
 
+{% endmacro %}
+
+{% macro recover_partitions(source_node) %}
+    {{ return(adapter.dispatch('recover_partitions', 'dbt_external_tables')(source_node)) }}
 {% endmacro %}
