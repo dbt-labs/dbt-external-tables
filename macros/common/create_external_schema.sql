ba2191ebe67e2489ceba4b88d9b10381c582ced5
guillesd <guillermo.sanchez@ah.nl>
2022-09-23 15:32:47 +0200

    add default create_external_schema macro

--- macros/common/create_external_schema.sql
+++ macros/common/create_external_schema.sql
@@ -1,9 +1,11 @@
 {% macro create_external_schema(source_node) %}
-    {{ adapter.dispatch('create_schema', 'dbt_external_tables')(source_node) }}
+    {{ adapter.dispatch('create_external_schema', 'dbt_external_tables')(source_node) }}
 {% endmacro %}
 
-{% macro default__create_external_schema() %}
-    {{ exceptions.raise_compiler_error(
-        "Dropping external tables is not implemented for the default adapter"
-    ) }}
+{% macro default__create_external_schema(source_node) %}
+    {% set ddl %}
+        create schema if not exists {{ source_node.schema }}
+    {% endset %}
+
+    {{return(ddl)}}
 {% endmacro %}
