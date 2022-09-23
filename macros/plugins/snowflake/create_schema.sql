4da73ad8da70792701c10067717b7f13c30a1dde
guillesd <guillermo.sanchez@ah.nl>
2022-09-23 12:00:03 +0200

    add implementation of create_schema for snowflake

---
+++ macros/plugins/snowflake/create_schema.sql
@@ -0,0 +1,10 @@
+{% macro snowflake__create_schema(source_node) %}
+    {# https://docs.snowflake.com/en/sql-reference/sql/create-schema.html #}
+
+    {% set ddl %}
+        create schema if not exists {{ source_node.schema }}
+    {% endset %}
+
+    {{return(ddl)}}
+
+{% endmacro %}
