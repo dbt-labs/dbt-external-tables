a9414fede9a9cb321518ca41f8b79bbef3915098
guillesd <guillermo.sanchez@ah.nl>
2022-09-23 11:59:18 +0200

    add implementation of create_schema for bigquery

---
+++ macros/plugins/bigquery/create_schema.sql
@@ -0,0 +1,10 @@
+{% macro snowflake__create_schema(source_node) %}
+    {# https://cloud.google.com/bigquery/docs/reference/standard-sql/data-definition-language#create_schema_statement #}
+
+    {% set ddl %}
+        create schema if not exists {{ source_node.schema }}
+    {% endset %}
+
+    {{return(ddl)}}
+
+{% endmacro %}
