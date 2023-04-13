e001ab831156e6f13dbbc71bfc65f97d10256764
Elize Papineau <elize.papineau@dbtlabs.com>
2023-04-13 14:37:13 -0700

    Conditionally generate fqn by source contents~

--- macros/common/create_external_schema.sql
+++ macros/common/create_external_schema.sql
@@ -3,8 +3,15 @@
 {% endmacro %}
 
 {% macro default__create_external_schema(source_node) %}
+    {% set fqn %}
+        {%- if source_node.database is not none -%}
+            {{ source_node.database }}.{{ source_node.schema }}
+        {%- else -%}
+            {{ source_node.schema }}
+    {% endset %}
+
     {% set ddl %}
-        create schema if not exists {{ source_node.database }}.{{ source_node.schema }}
+        create schema if not exists {{ fqn }}
     {% endset %}
 
     {{ return(ddl) }}
