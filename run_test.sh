4c56979c81a7190e6c752a19dfde4d1436d866d8
jeremyyeo <jeremyyeo@gmail.com>
2023-04-14 10:50:18 +1200

    update test command

--- run_test.sh
+++ run_test.sh
@@ -34,6 +34,6 @@ echo "Starting integration tests"
 dbt deps --target $1
 dbt seed --full-refresh --target $1
 dbt run-operation prep_external --target $1
-dbt run-operation stage_external_sources --var 'ext_full_refresh: true' --target $1
+dbt run-operation stage_external_sources --vars 'ext_full_refresh: true' --target $1
 dbt run-operation stage_external_sources --target $1
 dbt test --target $1
