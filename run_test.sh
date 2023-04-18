d18d3bbbae24d9fc0e8f51893b59c179982ed0b8
jeremyyeo <jeremyyeo@gmail.com>
2023-04-18 19:23:00 +1200

    move set

--- run_test.sh
+++ run_test.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
-set -eo pipefail
 
 echo "Setting up virtual environment"
 VENV="venv/bin/activate"
 
 if [[ ! -f $VENV ]]; then
-    # sudo apt update
-    # sudo apt install python3.8-venv
-    # python3.8 -m venv venv
-    # . $VENV
-    pip3 install --upgrade pip setuptools
+    python3.8 -m venv venv
+    . $VENV
+    pip install --upgrade pip setuptools
     if [ $1 == 'databricks' ]
     then
         echo "Installing dbt-spark"
-        pip3 install dbt-spark[ODBC] --upgrade --pre
+        pip install dbt-spark[ODBC] --upgrade --pre
     elif [ $1 == 'azuresql' ]
     then
         echo "Installing dbt-sqlserver"
-        pip3 install dbt-sqlserver --upgrade --pre
+        pip install dbt-sqlserver --upgrade --pre
     else
         echo "Installing dbt-$1"
-        pip3 install dbt-$1 --upgrade --pre
+        pip install dbt-$1 --upgrade --pre
     fi
 fi
 
-# . $VENV
+. $VENV
 echo "Changing working directory: integration_tests"
 cd integration_tests
 
@@ -35,6 +32,7 @@ if [[ ! -e ~/.dbt/profiles.yml ]]; then
 fi
 
 echo "Starting integration tests"
+set -eo pipefail
 dbt deps --target $1
 dbt seed --full-refresh --target $1
 dbt run-operation prep_external --target $1
