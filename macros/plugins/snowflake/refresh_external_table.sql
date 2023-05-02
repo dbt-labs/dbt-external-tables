53ee4024e57d27e4869f09ccd0cd197e13327f2c
Wesley Batista <wsantos@babbel.com>
2023-05-02 14:19:58 +0200

    enable `ALTER ... REFRESH`
    Check `auto_refresh` is false only to evaluate a "manual_refresh", instead of requiring partitions to be set

--- macros/plugins/snowflake/refresh_external_table.sql
+++ macros/plugins/snowflake/refresh_external_table.sql
@@ -1,14 +1,7 @@
 {% macro snowflake__refresh_external_table(source_node) %}
 
     {% set external = source_node.external %}
-    {% set snowpipe = source_node.external.get('snowpipe', none) %}
-    
-    {% set auto_refresh = external.get('auto_refresh', false) %}
-    {% set partitions = external.get('partitions', none) %}
-    
-    {% set manual_refresh = (partitions and not auto_refresh) %}
-    
-    {% if manual_refresh %}
+    {% if not external.get('auto_refresh', false) %}
 
         {% set ddl %}
         begin;
