a4d7d688308b98c6d98b137667f87ab96f89b8a7
Elize Papineau <elizepapineau@gmail.com>
2023-05-21 15:46:19 -0700

    Add source node database (#196)

--- macros/plugins/bigquery/create_external_schema.sql
+++ macros/plugins/bigquery/create_external_schema.sql
@@ -1,11 +1,7 @@
-{%- macro create_external_schema(source_node) -%}
-    {{ adapter.dispatch('create_external_schema', 'dbt_external_tables')(source_node) }}
-{%- endmacro -%}
-
-{%- macro default__create_external_schema(source_node) -%}
+{%- macro bigquery__create_external_schema(source_node) -%}
     {%- set fqn -%}
         {%- if source_node.database -%}
-            {{ source_node.database }}.{{ source_node.schema }}
+            `{{ source_node.database }}`.{{ source_node.schema }}
         {%- else -%}
             {{ source_node.schema }}
         {%- endif -%}
