6b6fbde4112428fa817e32805c5d3684956f5255
Benoit Perigaud <8754100+b-per@users.noreply.github.com>
2023-06-06 10:37:03 +0200

    Add support to infer schemas on Snowflake (#211)
    * Add support to infer schemas on Snowflake
    * Add tests and example for Snowflake infer schema

--- integration_tests/macros/plugins/snowflake/prep_external.sql
+++ integration_tests/macros/plugins/snowflake/prep_external.sql
@@ -1,18 +1,22 @@
 {% macro snowflake__prep_external() %}
 
     {% set external_stage = target.schema ~ '.dbt_external_tables_testing' %}
+    {% set parquet_file_format = target.schema ~ '.dbt_external_tables_testing_parquet' %}
 
-    {% set create_external_stage %}
+    {% set create_external_stage_and_file_format %}
     
         begin;
         create or replace stage
             {{ external_stage }}
             url = 's3://dbt-external-tables-testing';
+        
+        create or replace file format {{ parquet_file_format }} type = parquet;
         commit;
             
     {% endset %}
 
     {% do log('Creating external stage ' ~ external_stage, info = true) %}
-    {% do run_query(create_external_stage) %}
+    {% do log('Creating parquet file format ' ~ parquet_file_format, info = true) %}
+    {% do run_query(create_external_stage_and_file_format) %}
     
 {% endmacro %}
--- macros/plugins/snowflake/create_external_table.sql
+++ macros/plugins/snowflake/create_external_table.sql
@@ -3,26 +3,47 @@
     {%- set columns = source_node.columns.values() -%}
     {%- set external = source_node.external -%}
     {%- set partitions = external.partitions -%}
+    {%- set infer_schema = external.infer_schema -%}
+
+    {% if infer_schema %}
+        {% set query_infer_schema %}
+            select * from table( infer_schema( location=>'{{external.location}}', file_format=>'{{external.file_format}}') )
+        {% endset %}
+        {% if execute %}
+            {% set columns_infer = run_query(query_infer_schema) %}
+        {% endif %}
+    {% endif %}
 
     {%- set is_csv = dbt_external_tables.is_csv(external.file_format) -%}
 
 {# https://docs.snowflake.net/manuals/sql-reference/sql/create-external-table.html #}
 {# This assumes you have already created an external stage #}
     create or replace external table {{source(source_node.source_name, source_node.name)}}
-    {%- if columns or partitions -%}
+    {%- if columns or partitions or infer_schema -%}
     (
         {%- if partitions -%}{%- for partition in partitions %}
-            {{partition.name}} {{partition.data_type}} as {{partition.expression}}{{- ',' if not loop.last or columns|length > 0 -}}
+            {{partition.name}} {{partition.data_type}} as {{partition.expression}}{{- ',' if not loop.last or columns|length > 0 or infer_schema -}}
         {%- endfor -%}{%- endif -%}
-        {%- for column in columns %}
-            {%- set column_quoted = adapter.quote(column.name) if column.quote else column.name %}
-            {%- set col_expression -%}
-                {%- set col_id = 'value:c' ~ loop.index if is_csv else 'value:' ~ column_quoted -%}
-                (case when is_null_value({{col_id}}) or lower({{col_id}}) = 'null' then null else {{col_id}} end)
-            {%- endset %}
-            {{column_quoted}} {{column.data_type}} as ({{col_expression}}::{{column.data_type}})
-            {{- ',' if not loop.last -}}
-        {% endfor %}
+        {%- if not infer_schema -%}
+            {%- for column in columns %}
+                {%- set column_quoted = adapter.quote(column.name) if column.quote else column.name %}
+                {%- set col_expression -%}
+                    {%- set col_id = 'value:c' ~ loop.index if is_csv else 'value:' ~ column_quoted -%}
+                    (case when is_null_value({{col_id}}) or lower({{col_id}}) = 'null' then null else {{col_id}} end)
+                {%- endset %}
+                {{column_quoted}} {{column.data_type}} as ({{col_expression}}::{{column.data_type}})
+                {{- ',' if not loop.last -}}
+            {% endfor %}
+        {% else %}
+        {%- for column in columns_infer %}
+                {%- set col_expression -%}
+                    {%- set col_id = 'value:' ~ column[0] -%}
+                    (case when is_null_value({{col_id}}) or lower({{col_id}}) = 'null' then null else {{col_id}} end)
+                {%- endset %}
+                {{column[0]}} {{column[1]}} as ({{col_expression}}::{{column[1]}})
+                {{- ',' if not loop.last -}}
+            {% endfor %}
+        {%- endif -%}
     )
     {%- endif -%}
     {% if partitions %} partition by ({{partitions|map(attribute='name')|join(', ')}}) {% endif %}
