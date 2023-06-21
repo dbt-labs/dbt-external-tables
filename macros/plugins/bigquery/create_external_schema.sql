ca8771de2159e62a7a9e4f7b436b5e1136f7ef21
Thomas van Latum <tvanlatum@sligro.nl>
2023-06-21 21:16:03 +0200

    fix check schema

--- macros/plugins/bigquery/create_external_schema.sql
+++ macros/plugins/bigquery/create_external_schema.sql
@@ -7,9 +7,20 @@
         {%- endif -%}
     {%- endset -%}
 
-    {%- set ddl -%}
-        create schema if not exists {{ fqn }}
-    {%- endset -%}
+    {% set schema_exists_query %}
+        select * from {{ source_node.database }}.INFORMATION_SCHEMA.SCHEMATA where schema_name = '{{ source_node.schema }}' limit 1
+    {% endset %}
+    {% if execute %}
+        {% set schema_exists = run_query(schema_exists_query)|length > 0 %}
+    {% else %}
+        {% set schema_exists = false %}
+    {% endif %}  
+
+    {%- if not schema_exists -%}
+        {%- set ddl -%}
+            create schema if not exists {{ fqn }}
+        {%- endset -%}
+    {%- endif -%}
 
     {{ return(ddl) }}
 {%- endmacro -%}
