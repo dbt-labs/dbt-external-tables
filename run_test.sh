104646727efdd6ec74ada62d6d5ccd6fbe0e5e50
Thomas van Latum <thomas.v.latum@gmail.com>
2024-03-01 09:53:07 +0100

    Add comment

--- run_test.sh
+++ run_test.sh
@@ -18,6 +18,7 @@ if [[ ! -f $VENV ]]; then
     else
         echo "Installing dbt-$1"
         pip install dbt-$1 --upgrade --pre
+        # Workaround untill the DBT 1.7.9 is released on PyPI
         pip install protobuf==4.25.3
     fi
 fi
