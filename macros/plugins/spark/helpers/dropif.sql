33d3da564dce8d0f86d8d62dfd67cd38376d9b8f
Jeremy Yeo 2 <116316213+jeremyyeo-dbt@users.noreply.github.com>
2024-04-05 14:37:05 +1300

    Delete macros/plugins/spark directory

--- macros/plugins/spark/helpers/dropif.sql
+++
@@ -1,9 +0,0 @@
-{% macro spark__dropif(node) %}
-    
-    {% set ddl %}
-        drop table if exists {{source(node.source_name, node.name)}}
-    {% endset %}
-    
-    {{return(ddl)}}
-
-{% endmacro %}
--- macros/plugins/spark/refresh_external_table.sql
+++
@@ -1,9 +0,0 @@
-{% macro spark__refresh_external_table(source_node) %}
-    
-    {% set refresh %}
-        refresh table {{source(source_node.source_name, source_node.name)}}
-    {% endset %}
-    
-    {% do return([refresh]) %}
-
-{% endmacro %}
